0d357734b7586250d8da4d464e27acaefd3b1b37
maher <gomemahero.com>
2022-05-12 20:27:45 +0300

    sliderrr one one one by slice when pge +1 ------> great

--- src/components/section3Tab/index.js
+++ src/components/section3Tab/index.js
@@ -1,92 +1,70 @@
 import React from "react";
 import CardComp from "./card";
-import { useEffect,useState } from "react";
-import './index.css'
-import { Card, Col, Row } from 'antd';
-import { motion } from "framer-motion";
-import { useDispatch,useSelector } from "react-redux";
-import{ tab3fetchAll     , tab3FilterFunc  ,  tabs3CategoryChange} from "../../redux/global";
+import { useEffect, useState } from "react";
+import "./index.css";
+import { Card, Col, Row } from "antd";
+import { motion } from "framer-motion"; 
+import { useDispatch, useSelector } from "react-redux";
+import {
+  tab3fetchAll,
+  tab3FilterFunc,
+  tabs3CategoryChange,
+} from "../../redux/global";
 import { data as electronics } from "./data"; // change data name to electronics console.log("electronics",electronics);
 const TabSection = () => {
+  const [page, setPage] = useState(0);
+  const dispatch = useDispatch();
+  const [tabs, setTabs] = useState(electronics.slice(page, page + 4));
 
+  // when click page slice from 0 to 4 and set to tabs state
 
-const [page, setPage] = useState(0);
-const dispatch = useDispatch();
-const [tabs, setTabs] = useState([]);
+  const handlePageChange = () => {
+    console.log("clicked----->");
 
-// when click page slice from 0 to 4 and set to tabs state
+    setPage(page + 1);
 
-    const handlePageChange = () => {
-        console.log("clicked----->");
+    console.log("page", page, page + 4, "-----", electronics.length);
 
-        setPage(page + 1);
-       
-console.log("page",page, page + 4,'-----',electronics.length);
+    if (page + 4 < electronics.length) {  // still slice to  electronics.length 
+      const sliced = electronics.slice(page, page + 4);
+      return setTabs(sliced); // all products after sliced
+    } else {   // if page + 4 > electronics.length  restart page to 0 and still slice to electronics.length
+      setPage(0);
 
-if (page + 4 < electronics.length) {
+      const sliced = electronics.slice(page, page + 4);
 
-        const sliced = electronics.slice(page , page +4);
-        return setTabs(sliced); // all products after sliced
-} else {
-
-    setPage(0);
-    
-
-    const sliced = electronics.slice(page , page +4);
-
-    return setTabs(sliced); // all products after sliced
-
-}
-
-
-
-
-
+      return setTabs(sliced); // all products after sliced
     }
+  };
 
+  const { tab3, tabs3Filter, tabs3Category } = useSelector(
+    (state) => state.global
+  );
 
-const { tab3,tabs3Filter,  tabs3Category } = useSelector( (state) => state.global );
+  useEffect(() => {
+    dispatch(tab3fetchAll(electronics)); // fetch all electronics products
 
-useEffect(() => {
+    // execute when the category is changed and change the category products filtered
 
-dispatch(tab3fetchAll(electronics));   // fetch all electronics products
-
-
-// execute when the category is changed and change the category products filtered
-
-dispatch(tab3FilterFunc(tabs3Category));    // category name sent to filter products 
-
-
-
-} , [tabs3Category]);   // when category changes change filtered products
-    
-
+    dispatch(tab3FilterFunc(tabs3Category)); // category name sent to filter products
+  }, [tabs3Category]); // when category changes change filtered products
 
   return (
     <div>
-      <h1 onClick={ handlePageChange} className="text-center font-bold text-2xl pt-[20px] h-[66px] hello">
+      <h1
+        onClick={handlePageChange}
+        className="text-center font-bold text-2xl pt-[20px] h-[66px] hello"
+      >
         {" "}
-        Tabs{" "}  {tabs3Category} {tabs3Filter.length}   <span className="ml-[20px] bg-green-200">{page}</span>
+        Tabs {tabs3Category} {tabs3Filter.length}{" "}
+        <span className="ml-[20px] bg-green-200">{page}</span>
       </h1>
 
       <div className="w-[100%] grid  lg:grid-cols-12 sm:grid-cols-6 gap-3  first:ml-4 last:mr-[50px]">
-       
-
-
-
-          { tabs.map((product, index) => {
-            return  <CardComp product={product} />
-
-
-
-
-
-
-
-            
-          })}
-      
-        </div>
+        {tabs.map((product, index) => {
+          return <CardComp product={product} />;
+        })}
+      </div>
       {/* </div> */}
     </div>
   );
